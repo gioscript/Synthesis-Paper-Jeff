9dad03e59a66fab8ef3130c8852a14a5b103730d
IASTATE\gio <giorgichi@gmail.com>
2018-10-26 17:13:07 -0500

    Add script to download Google Sheets

---
+++ 01_data_download.R
@@ -0,0 +1,44 @@
+library(googlesheets)
+library(tidyverse)
+library(readxl)
+
+
+
+# Download Google Sheets --------------------------------------------------
+
+# download TD Site Metadata + History -------------------------------------
+
+gap_site_history <- gs_key("1oZ2NEmoa0XHSGTWKaBLt0DJK1kIbpWO6iSZ0I2OE2gA")
+
+# save site metadata on local drive
+gs_download(from =  gap_site_history,
+            to = "./Data/Site_History.xlsx")
+
+
+
+# Download on-site weather data -------------------------------------------
+
+# get list of all google sheets that ends with "Weather" in its title
+weather_sheets_all <- gs_ls("Weather$")[1]
+
+# get list of sites
+read_xlsx("Data/Site_History.xlsx") %>%
+  # remove first row containing units
+  slice(-1) %>% 
+  select(UniqueID, "Drainage Retention Practice") %>%
+  # select only controlled drainage sites
+  filter(`Drainage Retention Practice` == "Controlled Drainage") %>%
+  mutate(sheet_title = paste(UniqueID, "Weather")) %>%
+  # find sites form TD that have google sheets for weather
+  inner_join(weather_sheets_all, by = "sheet_title") %>%
+  .[["sheet_title"]] -> weather_sheet_titles
+
+# save on-site weather files on local drive
+for (i in seq_along(weather_sheet_titles)) {
+  print(i)
+  gs_download(from = gs_title(weather_sheet_titles[i]),
+              to = paste0("Data/On-Site-Weather/", weather_sheet_titles[i], ".xlsx"),
+              overwrite = TRUE)
+  }
+
+
