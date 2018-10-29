addcb56c4f2a4467f08b9601f617ea39ee5e04e9
IASTATE\gio <giorgichi@gmail.com>
2018-10-29 17:36:56 -0500

    download yield data

--- 01_data_download.R
+++ 01_data_download.R
@@ -6,7 +6,7 @@ library(readxl)
 
 # Download Google Sheets --------------------------------------------------
 
-# download TD Site Metadata + History -------------------------------------
+# Download TD Site Metadata + History -------------------------------------
 
 gap_site_history <- gs_key("1oZ2NEmoa0XHSGTWKaBLt0DJK1kIbpWO6iSZ0I2OE2gA")
 
@@ -35,10 +35,35 @@ read_xlsx("Data/Site_History.xlsx") %>%
 
 # save on-site weather files on local drive
 for (i in seq_along(weather_sheet_titles)) {
-  print(i)
+  print(weather_sheet_titles[i])
   gs_download(from = gs_title(weather_sheet_titles[i]),
               to = paste0("Data/On-Site-Weather/", weather_sheet_titles[i], ".xlsx"),
               overwrite = TRUE)
   }
 
 
+
+# Download on-site yield data ---------------------------------------------
+
+# get list of all google sheets that ends with "Crop Yield Data" in its title
+yield_sheets_all <- gs_ls("Crop Yield Data$")[1]
+
+# get list of sites
+read_xlsx("Data/Site_History.xlsx") %>%
+  # remove first row containing units
+  slice(-1) %>% 
+  select(UniqueID, "Drainage Retention Practice") %>%
+  # select only controlled drainage sites
+  filter(`Drainage Retention Practice` == "Controlled Drainage") %>%
+  mutate(sheet_title = paste(UniqueID, "Crop Yield Data")) %>%
+  # find sites form TD that have google sheets for yield
+  inner_join(yield_sheets_all, by = "sheet_title") %>%
+  .[["sheet_title"]] -> yield_sheet_titles
+
+# save on-site weather files on local drive
+for (i in seq_along(yield_sheet_titles)) {
+  print(yield_sheet_titles[i])
+  gs_download(from = gs_title(yield_sheet_titles[i]),
+              to = paste0("Data/Yield/", yield_sheet_titles[i], ".xlsx"),
+              overwrite = TRUE)
+}
