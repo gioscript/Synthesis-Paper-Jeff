ffb15166491f5cf44c407149705728d43367cee1
IASTATE\gio <giorgichi@gmail.com>
2018-10-30 15:04:22 -0500

    combine precip and plot metadata with yield

--- 03_yield.R
+++ 03_yield.R
@@ -44,17 +44,60 @@ for (i in seq_along(yield_sheet_titles)) {
 
 
 # combine yield data for all sites and years
-yield_unformated %>% 
+yield <-
+  yield_unformated %>% 
   combine() %>% 
   bind_rows() %>% 
-  # mselect only yield data
+  # select only yield data
   select(siteid, plotid = "Plot ID", year, n_treatment = "N-treatment Corn", matches("yield")) %>%
-  gather(key = varname, value = value, -(siteid:n_treatment)) %>% 
-  separate(varname, into = c("code", "crop", "defenition"), sep = " ", remove = FALSE, extra = "merge") %>%
-  filter(str_detect(varname, "kg/ha") & crop != "Soybean" & !is.na(value))
-  distinct(varname) 
+  # remove decimal point at the end of the plots with numeric names (101.0, etc.)
+  mutate(plotid = str_replace(plotid, ".0$", "")) %>%
+  # tranform to long table
+  gather(key = varname, value = yield, -(siteid:n_treatment)) %>% 
+  separate(varname, into = c("code", "crop"), sep = " ", remove = FALSE, extra = "drop") %>%
+  mutate(yield = as.numeric(yield),
+         year = as.numeric(year)) %>%
+  filter(!is.na(yield)) %>%
+  select(siteid, plotid, year, n_treatment, crop, varname, yield) %>%
+  # separate sub-plot id from plot id for HICKS_B
+  mutate(subplotid = ifelse(siteid == "HICKS_B", plotid, NA),
+         plotid = ifelse(siteid == "HICKS_B", str_sub(plotid, 1, 2), plotid)) %>%
+  select(siteid, plotid, subplotid, everything())
 
-# HICKS_B has plot and sub-plot level yield, hence different PlotID needs to be accounted
 
 
-# after reading need to combine with plot treatment data
+# Summarise crop yield data -----------------------------------------------
+
+# combine yield data with plot dwm treatment
+yield_with_dwm_treatment <-
+  yield %>%
+  # add dwm treatment
+  left_join(plot_dwm_treatment, by = c("siteid", "plotid", "year")) %>%
+  left_join(plot_identifier, by = c("siteid", "plotid")) %>%
+  arrange(siteid, year, plotid) %>%
+  select(siteid, plotid, subplotid, DWM, year, n_treatment, dwm_treatment, crop, yield) %>%
+  # change "varies" with actual dwm treatment
+  mutate(DWM = ifelse(DWM == "varies" & dwm_treatment == "CD", "controlled drainage", DWM),
+         DWM = ifelse(DWM == "varies" & dwm_treatment == "FD", "free drainage", DWM)) %>%
+  select(-ends_with("treatment")) %>%
+  rename(dwm = DWM)
+
+
+# add monthly precipitation
+yield_with_dwm_and_precip <-
+  weather_monthly %>%
+  select(siteid, year, month, precip_30 = precip_30_year_monthly_ave, precip_diff) %>%
+  filter(month %in% c("May", "Jun", "Jul", "Aug")) %>%
+  gather(key = variable, value = value, precip_30:precip_diff) %>%
+  unite(temp, month, variable) %>%
+  spread(key = temp, value = value) %>%
+  select(siteid, year, May_precip_30, May_precip_diff, Jun_precip_30, Jun_precip_diff,
+         Jul_precip_30, Jul_precip_diff, Aug_precip_30, Aug_precip_diff) %>%
+  right_join(yield_with_dwm_treatment, by = c("siteid", "year")) %>% 
+  select(siteid, plotid, subplotid, dwm, year, crop, yield, everything())
+  
+
+# save data as CSV file
+yield_with_dwm_and_precip %>%
+  mutate_at(vars(yield:Aug_precip_diff), round, 1) %>%
+  write_csv(paste0("Output/Data/yield_", Sys.Date(), ".csv"), na = "")
